c537ab0a9f2412cd871e12a48d875847f70616f6
Daito-Non <0up567503251d4n@au.com>
2025-01-16 14:46:23 +0900

    キャラクターのWASDでの移動とspaceでのジャンプの追加

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        // Rigidbodyコンポーネントを取得
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // WASDキーによる移動
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
+
+        // Spaceキーによるジャンプ
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    // 地面に接触しているかを判定
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
